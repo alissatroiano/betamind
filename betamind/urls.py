1bcbebd871a8911d4e073accafe3559e7f042bfb
Atinos31 <atinos31.dev@gmail.com>
2022-05-20 20:13:02 +0000

    team template and styling

--- betamind/urls.py
+++ betamind/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
