ebda369348a54797b24f34cd89089a29cc611c9c
Oliver Cadman <o.cadman@live.co.uk>
2022-05-20 16:38:18 +0100

    Create new profiles app, hooked up to installed apps

--- betamind/settings.py
+++ betamind/settings.py
@@ -39,13 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
-    #local
-    'chatbot',
-
     # Custom Apps
     'home',
     'core',
+    'profiles',
 
     # Allauth
     'allauth',
