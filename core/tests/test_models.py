2e6c67d22fc65e5bff5078a87d415964fbe26c78
Oliver Cadman <o.cadman@live.co.uk>
2022-05-20 08:55:54 +0100

    Added unit test for creating superuser

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -20,3 +20,14 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.username, username)
         self.assertTrue(user.check_password(password))
+
+    def test_create_superuser(self):
+        """Test Creating a superuser is successful"""
+
+        user = get_user_model().objects.create_superuser(
+            "testuser123",
+            "testpassword123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
