51dedafc61107d7c195b0fe705e2078f525d2c59
Oliver Cadman <o.cadman@live.co.uk>
2022-05-21 20:01:11 +0100

    View now handles POST requests

--- betamind/blog/views.py
+++ betamind/blog/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 from django.forms.models import modelformset_factory
 from .models import Mood, Post, Comment
 from .forms import PostForm
+from profiles.models import UserProfile
+from django.contrib.auth import get_user_model
 
 
 # @login_required
@@ -17,12 +20,32 @@ from .forms import PostForm
 @login_required
 def blog(request):
     """
-    Blog index page
+    Render blog index page, and handle POST requests to
+    add a blog to the page.
     """
+
+    current_user = get_user_model().objects.get(username=request.user)
+
+    if request.method == "POST":
+        post_form = PostForm(request.POST or None)
+        if post_form.is_valid():
+            form = post_form.save(commit=False)
+            form.post_sender = current_user
+            form.save()
+            return redirect(reverse("blog"))
+
     posts = Post.objects.all()
     moods = Mood.objects.all()
 
-    return render(request, 'blog.html', {'posts': posts, 'moods': moods})
+    post_form = PostForm()
+
+    context = {
+        'posts': posts,
+        'moods': moods,
+        'post_form': post_form
+    }
+
+    return render(request, 'blog.html', context)
 
 
 # @login_required
