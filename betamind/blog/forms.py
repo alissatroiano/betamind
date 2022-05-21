74ce5f7c68d75d81021bb872b3c805fd8ed35104
Oliver Cadman <o.cadman@live.co.uk>
2022-05-21 20:01:01 +0100

    Added widgets for title and content fields

--- betamind/blog/forms.py
+++ betamind/blog/forms.py
@@ -1,39 +1,42 @@
 from django import forms
 from .models import Mood, Post, Comment
-from .models import MOOD
 
-
-# class MoodForm(forms.ModelForm):
-#     class Meta:
-#         model = Mood
-#         fields = ['name', 'mood', 'slug']
-#         labels = {
-#             'name': 'Mood Name',
-#             'mood': 'Mood',
-#             'slug': 'Slug',
-#         }
-#         widgets = {
-#             'mood': forms.Select(attrs={'class': 'form-control'}),
-#         }
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField(
+        required=False,
+        label="How are you doing?",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "I need a hand."
+        })
+    )
+
     content = forms.CharField(
         required=False,
+        label="What's on your mind?",
         widget=forms.Textarea(attrs={
-            'rows':2,
+            'rows':5,
             'width':'100%',
             'class': 'form-control',
-            'placeholder': 'Enter Image/YouTube Video Url or a text quote'}))
+            'placeholder': 'Please let us know.'}))
+    
+    mood = forms.ModelChoiceField(
+        widget=forms.Select(attrs={"class": "form-select"}),
+        initial="Happy",
+        queryset=Mood.objects.all(),
+        label="Select Your Mood"
+    )
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'mood', 'slug']
+        fields = ['title', 'content', 'mood']
         labels = {
             'title': 'Title',
             'content': 'Content',
-            'mood': 'Mood',
-            'slug': 'Slug',
+            'mood': 'Select your Mood'
         }
         widgets = {
             'mood': forms.Select(attrs={'class': 'form-control'}),
         }
+
