d07806fcf4571f7953fe750221b32a2a913fe012
Oliver Cadman <o.cadman@live.co.uk>
2022-05-22 10:44:58 +0100

    Added validation methods to PostForm

--- betamind/blog/forms.py
+++ betamind/blog/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Mood, Post, Comment
+import re
 
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(
-        required=False,
+        required=True,
         label="How are you doing?",
         widget=forms.TextInput(attrs={
             "class": "form-control",
@@ -39,4 +41,33 @@ class PostForm(forms.ModelForm):
         widgets = {
             'mood': forms.Select(attrs={'class': 'form-control'}),
         }
+
+    
+    def clean_title(self):
+        """
+        Raise a validation error if additional_info
+        field consists of only numbers.
+        Uses regex pattern to compare against input's value.
+        """
+        title = self.cleaned_data["title"]
+        regex = "^[0-9]+$"
+        if re.match(regex, title):
+            raise ValidationError(
+                "Please enter words as well as numbers.")
+        else:
+            return title
+
+    def clean_content(self):
+        """
+        Raise a validation error if additional_info
+        field consists of only numbers.
+        Uses regex pattern to compare against input's value.
+        """
+        content = self.cleaned_data["content"]
+        regex = "^[0-9]+$"
+        if re.match(regex, content):
+            raise ValidationError(
+                "Please enter words as well as numbers.")
+        else:
+            return content
 
