43d1ee533b82ffe46907eaecc13a454083ae7548
Atinos31 <atinos31.dev@gmail.com>
2022-05-22 14:06:57 +0000

    styles101

--- betamind/betamind/settings.py
+++ betamind/betamind/settings.py
@@ -26,13 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if "DEVELOPMENT" not in os.environ:
+if "DEVELOPMENT" in os.environ:
     SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 else:
     SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if "DEVELOPMENT" not in os.environ:
+if "DEVELOPMENT" in os.environ:
     DEBUG = False
 else:
     DEBUG = True
