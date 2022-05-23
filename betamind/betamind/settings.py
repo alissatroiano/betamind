565f3eff59cda0a4d922cd80c322671eac863bdb
Alissa <alissatroianonyc@gmail.com>
2022-05-23 09:48:25 -0400

    Update settings

--- betamind/betamind/settings.py
+++ betamind/betamind/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.sites',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
 
     # Custom Apps
