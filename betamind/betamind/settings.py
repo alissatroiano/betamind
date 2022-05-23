828faa9e425c2427160bcf2c5fe7dab19bb0250a
Alissa <alissatroianonyc@gmail.com>
2022-05-23 06:51:26 -0400

    Fix static files

--- betamind/betamind/settings.py
+++ betamind/betamind/settings.py
@@ -192,8 +192,6 @@ LOGIN_REDIRECT_URL = "/"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_LOCATION = "/static/"
 
 MEDIA_URL = 'media/'
